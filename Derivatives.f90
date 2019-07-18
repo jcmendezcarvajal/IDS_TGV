e5a99a078c4078989388e6e6c9a142932741bdd1
Julio Mendez <jcmendez@aggies.ncat.edu>
2019-07-18 11:08:03 -0400

    Viscous part added for the momentum equations

--- Derivatives.f90
+++ Derivatives.f90
@@ -6,69 +6,89 @@ implicit none
 
 !$OMP PARALLEL PRIVATE (i,j)
 !$OMP DO
-do j =2,Jmax-1
-do i =2,Imax-1
-	! cell(a)
+  do j =2,Jmax-1
+    do i =2,Imax-1
+      ! cell(a)
 
-    dU1dt_a(i,j) =  -(E1_inv_a_right(i,j)-E1_inv_a_left(i,j))/ddx(i-1)      &
-                    -(F1_inv_a_upper(i,j)-F1_inv_a_lower(i,j))/ddy(j-1)
+      dU1dt_a(i,j) =  -(E1_inv_a_right(i,j)-E1_inv_a_left(i,j))/ddx(i-1)      &
+      -(F1_inv_a_upper(i,j)-F1_inv_a_lower(i,j))/ddy(j-1)
 
-    dU2dt_a(i,j) =  -(E2_inv_a_right(i,j)-E2_inv_a_left(i,j))/ddx(i-1)      &
-                     -(F2_inv_a_upper(i,j)-F2_inv_a_lower(i,j))/ddy(j-1)
+      dU2dt_a(i,j) =  -(E2_inv_a_right(i,j)-E2_inv_a_left(i,j))/ddx(i-1)      &
+      -(F2_inv_a_upper(i,j)-F2_inv_a_lower(i,j))/ddy(j-1)    &
+      +(E2_vis_a_right(i,j)+E2_vis_a_left(i,j))/ddx(i-1)      &
+      +(F2_vis_a_upper(i,j)+F2_vis_a_lower(i,j))/ddy(j-1)
 
+      dU3dt_a(i,j) =  -(E3_inv_a_right(i,j)-E3_inv_a_left(i,j))/ddx(i-1)      &
+      -(F3_inv_a_upper(i,j)-F3_inv_a_lower(i,j))/ddy(j-1)    &
+      +(E3_vis_a_right(i,j)+E3_vis_a_left(i,j))/ddx(i-1)      &
+      +(F3_vis_a_upper(i,j)+F3_vis_a_lower(i,j))/ddy(j-1)
 
-    dU3dt_a(i,j) =  -(E3_inv_a_right(i,j)-E3_inv_a_left(i,j))/ddx(i-1)      &
-                     -(F3_inv_a_upper(i,j)-F3_inv_a_lower(i,j))/ddy(j-1)
+       dU4dt_a(i,j) =  -(E4_inv_a_right(i,j)-E4_inv_a_left(i,j))/ddx(i-1)      &
+       -(F4_inv_a_upper(i,j)-F4_inv_a_lower(i,j))/ddy(j-1)    &
+       +(E4_vis_a_right(i,j)+E4_vis_a_left(i,j))/ddx(i-1)      &
+       +(F4_vis_a_upper(i,j)+F4_vis_a_lower(i,j))/ddy(j-1)
 
+      ! cell(b)
 
-	  dU4dt_a(i,j) =  -(E4_inv_a_right(i,j)-E4_inv_a_left(i,j))/ddx(i-1)      &
-                     -(F4_inv_a_upper(i,j)-F4_inv_a_lower(i,j))/ddy(j-1)
+      dU1dt_b(i,j) =  -(E1_inv_b_right(i,j) - E1_inv_b_left(i,j)) /ddx(i)       &
+      -(F1_inv_b_upper(i,j) - F1_inv_b_lower(i,j))/ddy(j-1)
 
+      dU2dt_b(i,j) =  -(E2_inv_b_right(i,j) - E2_inv_b_left(i,j)) /ddx(i)       &
+      -(F2_inv_b_upper(i,j) - F2_inv_b_lower(i,j))/ddy(j-1)       &
+      +(E2_vis_b_right(i,j) + E2_vis_b_left(i,j)) /ddx(i)       &
+      +(F2_vis_b_upper(i,j) + F2_vis_b_lower(i,j))/ddy(j-1)
 
-	! cell(b)
+      dU3dt_b(i,j) =  -(E3_inv_b_right(i,j)-E3_inv_b_left(i,j))/ddx(i)        &
+      -(F3_inv_b_upper(i,j)-F3_inv_b_lower(i,j))/ddy(j-1)    &
+      +(E3_vis_b_right(i,j)+E3_vis_b_left(i,j))/ddx(i)        &
+      +(F3_vis_b_upper(i,j)+F3_vis_b_lower(i,j))/ddy(j-1)
 
-    dU1dt_b(i,j) =  -(E1_inv_b_right(i,j) - E1_inv_b_left(i,j)) /ddx(i)       &
-                    -(F1_inv_b_upper(i,j) - F1_inv_b_lower(i,j))/ddy(j-1)
+       dU4dt_b(i,j) =  -(E4_inv_b_right(i,j)-E4_inv_b_left(i,j))/ddx(i)        &
+       -(F4_inv_b_upper(i,j)-F4_inv_b_lower(i,j))/ddy(j-1)    &
+       +(E4_vis_b_right(i,j)+E4_vis_b_left(i,j))/ddx(i)        &
+       +(F4_vis_b_upper(i,j)+F4_vis_b_lower(i,j))/ddy(j-1)
 
-    dU2dt_b(i,j) =  -(E2_inv_b_right(i,j) - E2_inv_b_left(i,j)) /ddx(i)       &
-                    -(F2_inv_b_upper(i,j) - F2_inv_b_lower(i,j))/ddy(j-1)
+      ! cell(c)
 
-
-	dU3dt_b(i,j) =  -(E3_inv_b_right(i,j)-E3_inv_b_left(i,j))/ddx(i)        &
-                     -(F3_inv_b_upper(i,j)-F3_inv_b_lower(i,j))/ddy(j-1)
-
-	dU4dt_b(i,j) =  -(E4_inv_b_right(i,j)-E4_inv_b_left(i,j))/ddx(i)        &
-                     -(F4_inv_b_upper(i,j)-F4_inv_b_lower(i,j))/ddy(j-1)
-
-	! cell(c)
-
-	dU1dt_c(i,j) =  -(E1_inv_c_right(i,j)-E1_inv_c_left(i,j))/ddx(i) &
-                     -(F1_inv_c_upper(i,j)-F1_inv_c_lower(i,j))/ddy(j)
+      dU1dt_c(i,j) =  -(E1_inv_c_right(i,j)-E1_inv_c_left(i,j))/ddx(i) &
+      -(F1_inv_c_upper(i,j)-F1_inv_c_lower(i,j))/ddy(j)
 
       dU2dt_c(i,j) =  -(E2_inv_c_right(i,j)-E2_inv_c_left(i,j))/ddx(i) &
-                     -(F2_inv_c_upper(i,j)-F2_inv_c_lower(i,j))/ddy(j)
+      -(F2_inv_c_upper(i,j)-F2_inv_c_lower(i,j))/ddy(j) &
+      +(E2_vis_c_right(i,j)+E2_vis_c_left(i,j))/ddx(i) &
+      +(F2_vis_c_upper(i,j)+F2_vis_c_lower(i,j))/ddy(j)
 
-	dU3dt_c(i,j) =  -(E3_inv_c_right(i,j)-E3_inv_c_left(i,j))/ddx(i) &
-                     -(F3_inv_c_upper(i,j)-F3_inv_c_lower(i,j))/ddy(i)
+      dU3dt_c(i,j) =  -(E3_inv_c_right(i,j)-E3_inv_c_left(i,j))/ddx(i) &
+      -(F3_inv_c_upper(i,j)-F3_inv_c_lower(i,j))/ddy(i) &
+      +(E3_vis_c_right(i,j)+E3_vis_c_left(i,j))/ddx(i) &
+      +(F3_vis_c_upper(i,j)+F3_vis_c_lower(i,j))/ddy(j)
 
-	dU4dt_c(i,j) =  -(E4_inv_c_right(i,j)-E4_inv_c_left(i,j))/ddx(i) &
-                     -(F4_inv_c_upper(i,j)-F4_inv_c_lower(i,j))/ddy(j)
+       dU4dt_c(i,j) =  -(E4_inv_c_right(i,j)-E4_inv_c_left(i,j))/ddx(i) &
+       -(F4_inv_c_upper(i,j)-F4_inv_c_lower(i,j))/ddy(j) &
+       +(E4_vis_c_right(i,j)+E4_vis_c_left(i,j))/ddx(i) &
+       +(F4_vis_c_upper(i,j)+F4_vis_c_lower(i,j))/ddy(j)
 
-	! cell(d)
+      ! cell(d)
 
-	dU1dt_d(i,j) =  -(E1_inv_d_right(i,j)-E1_inv_d_left(i,j))/ddx(i-1) &
-                     -(F1_inv_d_upper(i,j)-F1_inv_d_lower(i,j))/ddy(j)
+      dU1dt_d(i,j) =  -(E1_inv_d_right(i,j)-E1_inv_d_left(i,j))/ddx(i-1) &
+      -(F1_inv_d_upper(i,j)-F1_inv_d_lower(i,j))/ddy(j)
 
       dU2dt_d(i,j) = -(E2_inv_d_right(i,j)-E2_inv_d_left(i,j))/ddx(i-1) &
-                    -(F2_inv_d_upper(i,j)-F2_inv_d_lower(i,j))/ddy(j)
+      -(F2_inv_d_upper(i,j)-F2_inv_d_lower(i,j))/ddy(j) &
+      +(E2_vis_d_right(i,j)+E2_vis_d_left(i,j))/ddx(i-1) &
+      +(F2_vis_d_upper(i,j)+F2_vis_d_lower(i,j))/ddy(j)
 
-	dU3dt_d(i,j) =  -(E3_inv_d_right(i,j)-E3_inv_d_left(i,j))/ddx(i-1) &
-                     -(F3_inv_d_upper(i,j)-F3_inv_d_lower(i,j))/ddy(j)
+      dU3dt_d(i,j) =  -(E3_inv_d_right(i,j)-E3_inv_d_left(i,j))/ddx(i-1) &
+      -(F3_inv_d_upper(i,j)-F3_inv_d_lower(i,j))/ddy(j) &
+      +(E3_vis_d_right(i,j)+E3_vis_d_left(i,j))/ddx(i-1)  &
+      +(F3_vis_d_upper(i,j)+F3_vis_d_lower(i,j))/ddy(j)
 
-	dU4dt_d(i,j) =  -(E4_inv_d_right(i,j)-E4_inv_d_left(i,j))/ddx(i-1) &
-                     -(F4_inv_d_upper(i,j)-F4_inv_d_lower(i,j))/ddy(j)
-Enddo
-Enddo
+       dU4dt_d(i,j) =  -(E4_inv_d_right(i,j)-E4_inv_d_left(i,j))/ddx(i-1) &
+       -(F4_inv_d_upper(i,j)-F4_inv_d_lower(i,j))/ddy(j) &
+       +(E4_vis_d_right(i,j)+E4_vis_d_left(i,j))/ddx(i-1) &
+       +(F4_vis_d_upper(i,j)+F4_vis_d_lower(i,j))/ddy(j)
+    Enddo
+  Enddo
 !$OMP END DO
 
  ! Calculate the average derivative of solution flux (Um)
@@ -84,10 +104,11 @@ Enddo
  dU3dt(i,j)= 0.25*(dU3dt_a(i,j)+dU3dt_b(i,j)+ &
                       dU3dt_c(i,j)+dU3dt_d(i,j))
 
- dU4dt(i,j)= 1.0 !0.25*(dU4dt_a(i,j)+dU4dt_b(i,j)+ &
-                  !   dU4dt_c(i,j)+dU4dt_d(i,j))
 Enddo
 Enddo
 !$OMP END DO
 !$OMP END PARALLEL
+
+dU4dt = 1.0
+
 End Subroutine Derivatives
